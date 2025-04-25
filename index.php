408e074b34c68195ad7b571d8335f4a35aac6e4b
arysweb <sebastian.czinna@gmail.com>
2025-04-25 20:45:24 +0200

    1.0.0

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Bento Grid</title>
+  <link rel="stylesheet" href="css/style.css" />
+
+  <link href="https://fonts.googleapis.com/css2?family=Cal+Sans&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Sans:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <div class="bento-grid">
+        <div class="bento-card card-a">
+            <div class="bento-title title">Card A Title</div>
+            <div class="bento-desc">This is a reusable description for Card A.</div>
+        </div>
+        <div class="bento-card card-b">
+            <div class="bento-title title">Card B Title</div>
+            <div class="bento-desc">This is a reusable description for Card B.</div>
+        </div>
+        <div class="bento-card card-c">
+            <div class="bento-title title">Card C Title</div>
+            <div class="bento-desc">This is a reusable description for Card C.</div>
+        </div>
+        <div class="bento-card card-d">
+            <div class="bento-title title">Card D Title</div>
+            <div class="bento-desc">This is a reusable description for Card D.</div>
+        </div>
+        <div class="bento-card card-e">
+            <div class="bento-title title">Card E Title</div>
+            <div class="bento-desc">This is a reusable description for Card E.</div>
+        </div>
+        <div class="bento-card card-f">
+            <div class="bento-title title">Card F Title</div>
+            <div class="bento-desc">This is a reusable description for Card F.</div>
+        </div>
+        <div class="bento-card card-g">
+            <div class="bento-title title">Card G Title</div>
+            <div class="bento-desc">This is a reusable description for Card G.</div>
+        </div>
+    </div>
+</body>
+</html>
